ec2c441e244c9d9c8c03253ae1ed704bed84ed56
Kurt Van Etten <kurt@vanetten.us>
2019-12-17 21:40:38 -0500

    Completed code-along exercise

--- data/migrations/20190603182406_posts-schema.js
+++ data/migrations/20190603182406_posts-schema.js
@@ -1,12 +1,9 @@
-exports.up = (knex) => {
-  return knex.schema.createTable("posts", (posts) => {
-    posts.increments()
-    posts.string("title", 1024).notNullable()
-    posts.text("contents").notNullable()
-    posts.timestamps(true, true)
-  })
-}
+exports.up = knex =>
+  knex.schema.createTable('posts', posts => {
+    posts.increments();
+    posts.string('title', 1024).notNullable();
+    posts.text('contents').notNullable();
+    posts.timestamps(true, true);
+  });
 
-exports.down = (knex) => {
-  return knex.schema.dropTableIfExists("posts")
-}
+exports.down = knex => knex.schema.dropTableIfExists('posts');
--- posts/post-router.js
+++ posts/post-router.js
@@ -1,26 +1,72 @@
-const express = require("express")
-const db = require("../data/db-config") // database access using knex
+const express = require('express');
+const db = require('../data/db-config'); // database access using knex
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res, next) => {
+router.get('/', async (req, res, next) => {
+  try {
+    const post = await db('posts');
+    res.json(post);
+  } catch (err) {
+    next(err);
+  }
+});
 
-})
+router.get('/:id', async (req, res, next) => {
+  try {
+    const id = Number.parseInt(req.params.id);
+    const post = await db('posts').where('id', id);
+    res.json(post);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get("/:id", (req, res, next) => {
+router.post('/', async (req, res, next) => {
+  try {
+    const payload = {
+      title: req.body.title,
+      contents: req.body.contents,
+    };
+    const [id] = await db('posts').insert(payload);
+    const newPost = await db('posts')
+      .where('id', id)
+      .first();
+    res.json(newPost);
+  } catch (err) {
+    next(err);
+  }
+});
 
-})
+router.put('/:id', async (req, res, next) => {
+  try {
+    const payload = {
+      title: req.body.title,
+      contents: req.body.contents,
+    };
+    const id = Number.parseInt(req.params.id);
+    await db('posts')
+      .where('id', id)
+      .update(payload);
+    const updatedPost = await db('posts')
+      .where('id', id)
+      .first();
+    res.json(updatedPost);
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.post("/", (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const id = Number.parseInt(req.params.id);
+    const lines = await db('posts')
+      .where('id', id)
+      .del();
+    res.status(202).json({ 'records deleted': lines });
+  } catch (err) {
+    next(err);
+  }
+});
 
-})
-
-router.put("/:id", (req, res, next) => {
-
-})
-
-router.delete("/:id", (req, res, next) => {
-
-})
-
-module.exports = router
+module.exports = router;
